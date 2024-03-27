33608fb209990bf23899d597a5b8aa84093c8209
SelimBHa <141486892+SelimBHa@users.noreply.github.com>
2024-03-27 12:18:28 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -18,9 +18,9 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key in ('created_at', 'updated_at'):
-             setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                 setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
                 elif key != '__class__':
-                    setattr(self, key, value)
+                 setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
