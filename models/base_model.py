a9ba2c25e6355ba13b98aee5f00c7d8646436a65
SelimBHa <141486892+SelimBHa@users.noreply.github.com>
2024-03-27 12:07:52 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -17,7 +17,7 @@ class BaseModel:
         """Initializes a new instance of BaseModel."""
         if kwargs:
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
+                if key in ('created_at', 'updated_at'):
                     setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
                 elif key != '__class__':
                     setattr(self, key, value)
